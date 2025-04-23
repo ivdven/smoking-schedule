0904369973826024d7aa3f0232f730ba59dc4122
Immanuel van de Ven <immanuelvandeven@gmail.com>
2025-04-23 13:35:46 +0200

    Applied Pinia in main.js to replace store

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
+import pinia from './store/index'
 
 import '@/assets/main.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(pinia).use(router).mount('#app')
