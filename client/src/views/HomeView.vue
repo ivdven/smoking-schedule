f54eccda7440b15a64ab2cbedc1650347c937fad
Immanuel van de Ven <immanuelvandeven@gmail.com>
2025-04-23 14:16:56 +0200

    Applied authStore in HomeView for testing purposes

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <template>
-  <h1>Home</h1>
+  <p>Hi, {{ authStore.user.email }}</p>
 </template>
 
 <script>
+import { useAuthStore } from '../store/modules/authStore'
+
 export default {
   setup() {
     
+    const authStore = useAuthStore()
+
+    return {
+      authStore
+    }
   }
 }
 </script>
