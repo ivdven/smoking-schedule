10b2edd86a3674683feb05d4c308d2d413da5857
Immanuel van de Ven <immanuelvandeven@gmail.com>
2025-04-23 14:29:59 +0200

    Imported the userRoutes

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
+const userRoutes = require('./routes/auth/userRoutes')
 
 const connectMongo = require('./config/db')
 
 const app = express()
 const PORT = process.env.PORT
 
-connectMongo()
-
 app.use(cors())
 app.use(express.json())
+app.use('/api/users', userRoutes)
+
+connectMongo()
 
 app.listen(PORT, () => {
   console.log(`server running on port: http://localhost:${PORT}`)
