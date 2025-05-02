53f17df122064b67a1d90516df78fa91ae98b032
Immanuel van de Ven <immanuelvandeven@gmail.com>
2025-05-02 16:49:40 +0200

    Created the loginData variable for submitting reactive data. Also applied storeToRefs for rendering the errors to the frontend.

--- client/src/views/auth/LoginView.vue
+++ client/src/views/auth/LoginView.vue
@@ -7,13 +7,13 @@
         <label>E-mail</label>
         <input
           type="email"
-          v-model="user.email"
+          v-model="loginData.email"
           required
         >
         <label>Password</label>
         <input
           type="password"
-          v-model="user.password"
+          v-model="loginData.password"
           required
         >
         <div class="form-actions">
@@ -30,25 +30,33 @@
 <script>
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '../../store/modules/authStore'
+import { reactive } from 'vue'
+import { storeToRefs } from 'pinia'
 
 export default {
   setup() {
 
     const authStore = useAuthStore()
-    const { user, error, isPending } = authStore
     const router = useRouter()
+
+    const { error, isPending } = storeToRefs(authStore)
+
+    const loginData = reactive({
+      email: '',
+      password: '',
+    })
     
     const handleSubmit = async () => {
-      const res = await authStore.login(user.email, user.password)
-      if (!error && res) {
+      const res = await authStore.login(loginData.email, loginData.password)
+      if (!error.value && res) {
         router.push({ name: 'HomeView' })
       } else {
-        console.log('Login failed: ', authStore.error)
+        console.log('Login failed: ', error)
       }
     }
 
     return {
-      user,
+      loginData,
       error,
       isPending,
       handleSubmit
