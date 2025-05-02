a54e4ceb5980f38fd0bc7237f341d868bb7748b2
Immanuel van de Ven <immanuelvandeven@gmail.com>
2025-05-02 16:50:29 +0200

    Added some more code to test the login function

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,17 +1,31 @@
 <template>
-  <p>Hi, {{ authStore.user.email }}</p>
+  <div v-if="user">
+    <p>Hi, {{ user.firstName }}</p>
+  </div>
+  <button @click="handleLogOut">log out</button>
 </template>
 
 <script>
+import { useRouter } from 'vue-router'
 import { useAuthStore } from '../store/modules/authStore'
+import { computed } from 'vue'
 
 export default {
   setup() {
-    
+
+    const router = useRouter()
     const authStore = useAuthStore()
 
+    const user = computed(() => authStore.user)
+    
+    const handleLogOut = async () => {
+      authStore.logout()
+      router.push({ name: 'LoginView' })
+    }
+    
     return {
-      authStore
+      user,
+      handleLogOut
     }
   }
 }
