f2c5c1d48bb4cca97131c55a9d9aa42b44d04a62
Immanuel van de Ven <immanuelvandeven@gmail.com>
2025-06-10 17:19:19 +0200

    Modified the response to asign id to the userId

--- server/controllers/auth/getUserProfile.js
+++ server/controllers/auth/getUserProfile.js
@@ -4,7 +4,7 @@ const getProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user.userId).select('-password -__v').lean()
     if (!user) return res.status(404).json({ message: 'User not found' })
-    res.json(user)
+    res.json({ ...user, id: req.user.userId })
   } catch (err) {
     res.status(500).json({ message: 'Error retrieving profile' })
   }
