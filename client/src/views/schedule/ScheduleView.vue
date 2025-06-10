befaa8684a4a47a7683dbd7fae87fdaed4fa6e74
Immanuel van de Ven <immanuelvandeven@gmail.com>
2025-06-10 16:50:33 +0200

    Added ScheduleView.vue to view all schedules.

---
+++ client/src/views/schedule/ScheduleView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div><SchedulePicker /></div>
+</template>
+
+<script>
+import SchedulePicker from '@/components/SchedulePicker.vue'
+
+
+export default {
+  components: {
+    SchedulePicker
+  },
+  setup() {
+    return {
+      
+    }
+  }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
