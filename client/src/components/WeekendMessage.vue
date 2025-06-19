8566f52bd07098006aa287f80cc0d0328f2cb5b6
Immanuel van de Ven <immanuelvandeven@gmail.com>
2025-06-19 12:15:35 +0200

    Added WeekendMessage.vue component when it is not a work day.

---
+++ client/src/components/WeekendMessage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="window">
+    <div class="window-title">Weekend message</div>
+    <div class="card-container">
+      <p>No upcoming breaks, see you on Monday!</p>
+      <p>Have a nice weekend!</p>
+    </div>
+    <div class="button-container">
+      <button @click="handleLogOut">Log out</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useAuthStore } from '@/store/modules/authStore'
+import { useRouter } from 'vue-router'
+
+export default {
+  setup() {
+
+    const authStore = useAuthStore()
+    const router = useRouter()
+
+    const handleLogOut = async () => {
+      console.log('log out')
+      authStore.logout()
+      router.push({ name: 'LoginView' })
+    }
+    return {
+      handleLogOut
+    }
+  }
+}
+</script>
+
+<style>
+.card-container {
+  gap: 0px;
+}
+</style>
